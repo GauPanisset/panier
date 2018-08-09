0f56adc26af648b395407989cb5dd4f6f91dedc9
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-08-09 22:54:32 +0200

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const serveStatic = require('serve-static');
+const FS = require('fs');
 const PORT = process.env.PORT || 5000;
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-app.get('/home', function (req, res) {
-    res.sendFile(__dirname + '/dist')
-});
+console.log(FS.existsSync(__dirname + "/dist"));
 
 app.listen(PORT);
 console.log('server started '+ PORT);
