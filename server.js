4571198d6b4f77b5c0f4967d9c7eb08190a2aadb
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-08-09 16:42:30 +0200

    heroku

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const PORT = process.env.PORT || 5000;
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+});
+
 app.listen(PORT);
 console.log('server started '+ PORT);
