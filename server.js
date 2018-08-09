d3d7af52743fc73fadf2973f03f46afbb0d6b6e0
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-08-09 16:38:40 +0200

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const serveStatic = require('serve-static');
 const PORT = process.env.PORT || 5000;
 
 app = express();
-app.use(serveStatic(__dirname + "/dist/home.html"));
+app.use(serveStatic(__dirname + "/dist"));
 
 app.listen(PORT);
 console.log('server started '+ PORT);
