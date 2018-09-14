70a01ab3c75336b92f6e289303bb83eec1cdde18
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-09-14 13:58:53 +0200

    heroku

--- src/pages/home/app.vue
+++ src/pages/home/app.vue
@@ -46,28 +46,6 @@
                     myApp.style.overflowY = "hidden";
                 }
             },
-            getOS() {
-                var userAgent = window.navigator.userAgent,
-                    platform = window.navigator.platform,
-                    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-                    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-                    iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-                    os = null;
-
-                if (macosPlatforms.indexOf(platform) !== -1) {
-                    os = 'Mac OS';
-                } else if (iosPlatforms.indexOf(platform) !== -1) {
-                    os = 'iOS';
-                } else if (windowsPlatforms.indexOf(platform) !== -1) {
-                    os = 'Windows';
-                } else if (/Android/.test(userAgent)) {
-                    os = 'Android';
-                } else if (!os && /Linux/.test(platform)) {
-                    os = 'Linux';
-                }
-
-                return os;
-            },
         },
 
         computed: {
@@ -78,8 +56,7 @@
             document.getElementById("app").addEventListener('scroll', this.handleScroll);
             $('#app').on('mousewheel', function(event) {
                 if (that.position.scrollTop >= $(window).height()) {
-                    console.log(event.delta, event.deltaFactor);
-                    this.scrollLeft -= event.deltaY;
+                    this.scrollLeft -= event.deltaY * event.deltaFactor;
                 }
             });
         },
